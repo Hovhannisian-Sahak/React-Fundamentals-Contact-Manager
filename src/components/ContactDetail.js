79729fa8c558015dad99c0f171e9e74784b56722
SuperSkinny11 <eversuperskinny@gmail.com>
2023-03-28 12:18:29 +0400

    new changes

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -11,7 +11,7 @@ const ContactDetail = () => {
     <div>
       <div className="m-4">
         <Button success onClick={() => navigate("/contactList")}>
-          Back to contactList
+          Back to contacts
         </Button>
       </div>
 
